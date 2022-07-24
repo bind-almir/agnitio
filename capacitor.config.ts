7eb1d7df299b08bae14d0db30e32cc835a3ac2ed
Almir Zulic <almir@bind.ba>
2022-07-24 11:31:54 +0200

    add more attributes

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.almirzulic.agnitio',
   appName: 'agnitio',
   webDir: 'www',
   bundledWebRuntime: false
